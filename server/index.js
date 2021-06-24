cf9198613bbf27cb5ceabe7d5883330f65dec89b
Yash <ypt7895@gmail.com>
2021-06-25 00:28:45 +0530

    server username part

--- server/index.js
+++ server/index.js
@@ -3,11 +3,17 @@ const io = require('socket.io')(http,{
     cors:{origin:'*'}
 })
 
-var users = []
+var users = {}
 
 io.on('connection',(socket)=>{
     console.log('User connected')
-
+    socket.emit('userConnect','A new user has arrived')
+    socket.on('addUser',userName=>{
+        users[socket.id] = userName
+    })
+    socket.on('message',content=>{
+        io.emit('message', `${users[socket.id]}(${socket.id}) said ${content}`)
+    })
 })
 
 http.listen(8080,()=>console.log('App started'))
