e498755402f81d7663ad423d9b43a3c872c1f3bf
Yash <ypt7895@gmail.com>
2021-06-25 00:29:25 +0530

    client username part

--- client/client.js
+++ client/client.js
@@ -1,1 +1,17 @@
 const socket = io('ws://localhost:8080')
+socket.on('userConnect',receiveData=>{
+    const listElem = document.createElement('li');
+    listElem.innerText = receiveData;
+    document.querySelector('ul').appendChild(listElem);
+    socket.emit('addUser',prompt("Enter your name:"))
+})
+socket.on('message',text=>{
+    const listElem = document.createElement('li');
+    listElem.innerText = text
+    document.querySelector('ul').appendChild(listElem)
+})
+
+document.querySelector('button').onclick = () =>{
+    const text = document.querySelector('input').value
+    socket.emit('message',text)
+}
